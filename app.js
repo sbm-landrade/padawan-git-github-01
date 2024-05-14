8d654a1d99640633150d71c78d46c6f9c0a0f344
Lucas Andrade <lucas.andrade@sbmtechnology.com.br>
2024-05-14 14:18:27 -0300

    adicionando um console log

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ console.log("Rodando o sistema de gerenciamento de jogos");
 console.log("aplicação está se conectado ao banco de dados");
 // mais um console log de teste:
 console.log("O Walter fez esse commit sem eu ver");
+console.log("Enviando para o GitHub");
